c38f43a2001b76a69e256a6208bdc2f3b80de5de
camilajordana <c_jordana@hotmail.com>
2015-08-26 16:45:34 -0300

    Questoes

--- capital_teste.cpp
+++ capital_teste.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm> 
 using namespace std;
 
-int main (){
-	int a1 = 0;
-	int a2 = 0;
-	int a3 = 0;
-	int a4 = 0;
+int main(){
 
+	vector<double> valores(4);
+	for(int i = 0; i < 4; i++){
+		cin >> valores[i];
+	}
+	
+	sort(valores.begin(), valores.end());
+	
+	if((valores[1]/valores[0] == valores[3]/valores[2]) && (valores[2]/valores[0] == valores[3]/valores[1])){
+		cout << "S\n";
+	} else {
+		cout << "N\n";
+	}
 
 	return 0;
 }
